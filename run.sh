bc3cd9c599154d534613149e69e6670a4f73a95c
SGX-Search <SGX-Search@example.com>
2023-03-28 18:54:18 +0800

    add shell

---
+++ run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+make SGX_DEBUG=0
+/opt/intel/sgxsdk/bin/x64/sgx_sign sign -key Enclave/Enclave_private_test.pem -enclave enclave.so -out enclave.signed.so -config Enclave/Enclave.config.xml
+echo "-------------------------"
+./app
+make clean
